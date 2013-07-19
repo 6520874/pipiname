a665d0706974d8aa31dff02c01ceb45a3bb7f45c
6520874@163.com <6520874@163.com>
2013-07-19 10:21:46 +0800

    test
    Signed-off-by: 6520874@163.com <6520874@163.com>

--- war3Helper/war3HelperDlg.cpp
+++ war3Helper/war3HelperDlg.cpp
@@ -339,15 +339,13 @@ BOOL Cwar3HelperDlg::OnInitDialog()
 	//  执行此操作
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
-	m_hyperlink.SetURL(L"http://pipi.1kapp.com/?p=10");
+	m_hyperlink.SetURL(L"http://pipi.1kapp.com");
   
 	TCHAR strWar3Path[MAX_PATH] = {0};
 	GetPrivateProfileString(L"war3path",L"path",L" ",strWar3Path,256,m_strDir+_T("//war3set.ini"));
     m_war3path.SetWindowText(strWar3Path);
 	//检测是否全屏
     CheckFullScreen();
-	// TODO: 在此添加额外的初始化代码
-	//brush.CreateSolidBrush(RGB(255,0,0)); 
 	//---------------------------托盘显示---------------------------------//
 	m_nid.cbSize = (DWORD)sizeof(NOTIFYICONDATA);
 
