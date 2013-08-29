bbd5174dd7512357bc0d90b14d548b6913e1cdd8
pipi <6520874@163.com>
2013-08-29 23:54:23 +0800

    普通广告update exe

---
+++ csdb/Update/UpdateDlg.cpp
@@ -0,0 +1,252 @@
+
+// UpdateDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "Update.h"
+#include "UpdateDlg.h"
+#include <Wininet.h>
+#include <Mmsystem.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+// CUpdateDlg 对话框
+
+
+
+
+CUpdateDlg::CUpdateDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CUpdateDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CUpdateDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PROGRESS1, m_process);
+}
+
+BEGIN_MESSAGE_MAP(CUpdateDlg, CDialog)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+// CUpdateDlg 消息处理程序
+
+
+
+
+
+
+BOOL CUpdateDlg::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+    WinExec("kill.bat",SW_HIDE);
+	TCHAR  strExePath[MAX_PATH] = {0};
+	GetCurrentDirectory(MAX_PATH,strExePath);
+	m_CsExePath = strExePath;
+
+	TCHAR szValue[MAX_PATH] = {0};
+	GetPrivateProfileString(_T("War3version"),_T("UpdateExeUrl"),_T("http://pipihaha.sinaapp.com/qyk/Setup.exe"),szValue,MAX_PATH,m_CsExePath+_T("//war3set.ini"));
+
+    InternetGetFile(szValue,_T("Setup.exe"));
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+
+void CUpdateDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialog::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CUpdateDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialog::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CUpdateDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+int CUpdateDlg::InternetGetFile(CString szUrl,CString szFileName)
+{	
+	if(szUrl.IsEmpty() ||szFileName.IsEmpty())
+	{
+		return -1;
+	}
+
+	DWORD dwFlags;
+     ShowWindow(SW_SHOW);
+    m_process.SetRange(0,1726340 );
+ 	InternetGetConnectedState(&dwFlags, 0);
+	CHAR strAgent[64];
+	sprintf(strAgent, "Agent%ld", timeGetTime());
+	HINTERNET hOpen;
+	if(!(dwFlags & INTERNET_CONNECTION_PROXY))
+		hOpen = InternetOpenA(strAgent, INTERNET_OPEN_TYPE_PRECONFIG_WITH_NO_AUTOPROXY, NULL, NULL, 0);
+	else
+		hOpen = InternetOpenA(strAgent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	if(!hOpen)
+	{
+		//AfxMessageBox("Internet连接错误!");
+		return -1;
+	}
+ 
+	DWORD dwSize;
+	CHAR   szHead[] = "Accept: */*\r\n\r\n";
+	VOID* szTemp[16384];
+	HINTERNET  hConnect;
+	CFile file;
+ 
+	USES_CONVERSION;
+	if ( !(hConnect = InternetOpenUrlA ( hOpen,szUrl,szHead,
+		 lstrlenA (szHead), INTERNET_FLAG_DONT_CACHE | INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_RELOAD, 0)))
+	{
+	  // AfxMessageBox("不能打开该URL!");
+	   return -1;
+	}
+
+	if  (file.Open(szFileName,CFile::modeWrite|CFile::modeCreate)==FALSE )
+	{
+	   //AfxMessageBox("不能打开本地的文件!");
+	  return -1;
+	}
+
+	DWORD dwByteToRead = 0;
+	DWORD dwSizeOfRq = 4;
+	DWORD dwBytes = 0;
+
+    if (!HttpQueryInfo(hConnect, HTTP_QUERY_CONTENT_LENGTH | HTTP_QUERY_FLAG_NUMBER, 
+                  (LPVOID)&dwByteToRead, &dwSizeOfRq, NULL))
+	{
+		dwByteToRead = 0;
+	}
+
+
+	do
+	{
+		if (!InternetReadFile (hConnect, szTemp, 16384,  &dwSize))
+		{
+			//AfxMessageBox("读文件出错!");
+			file.Close();
+			return -1;
+		}
+		if (dwSize==0)
+			break;
+		else
+			file.Write(szTemp,dwSize);
+		dwBytes+=dwSize;
+		m_process.SetPos(dwBytes);
+		//dwn = dwBytes;
+
+	}while (TRUE);
+    
+
+	file.Close();
+	InternetCloseHandle(hOpen);
+
+	
+	WinExec(szFileName,SW_SHOW);
+	SendMessage(WM_CLOSE,0,0);
+	return 0;
+}
---
+++ csdb/Update/UpdateDlg.h
@@ -0,0 +1,39 @@
+
+// UpdateDlg.h : 头文件
+//
+
+#pragma once
+#include "afxcmn.h"
+
+
+// CUpdateDlg 对话框
+class CUpdateDlg : public CDialog
+{
+// 构造
+public:
+	CUpdateDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_UPDATE_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+	int InternetGetFile(CString szUrl,CString szFileName);
+
+	CString GetWebStieHtml(CString strUrl);
+private:
+	CProgressCtrl m_process;
+	CString     m_CsExePath;
+};
